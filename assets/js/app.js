215202f2f874c73129633ecf07a486b3c96fb790
funkybeppe <lucabarbero1996@gmail.com>
2022-08-03 15:57:37 +0000

    add game variables and main game functions

---
+++ assets/js/app.js
@@ -0,0 +1,42 @@
+const userScore = 0;
+const computerScore = 0;
+const userScore_span = document.getElementById('win');
+const computerScore_span = document.getElementById('lose');
+const resultDiv = document.querySelector('.result');
+const rockDiv = document.getElementById('r');
+const paperDiv = document.getElementById('p');
+const scissorsDiv = document.getElementById('s');
+const lizardDiv = document.getElementById('l');
+const spockDiv = document.getElementById('v');
+
+function getComputerCoice() {
+    const choices  = ['r','p','s','l','v'];
+}
+
+function game(userChoice) {
+
+}
+
+function main() {
+
+    rockDiv.addEventListener ('click', function() {
+        game('r')
+    });
+    
+    paperDiv.addEventListener ('click', function() {
+        game('p')
+    });
+    
+    scissorsDiv.addEventListener ('click', function() {
+        game('s')
+    });
+    
+    lizardDiv.addEventListener ('click', function() {
+        game('l')
+    });
+    
+    spockDiv.addEventListener ('click', function() {
+        game('v')   
+    });
+}
+main()
